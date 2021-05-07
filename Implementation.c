cb7652ce98ebd39f5a8190dc710ec7f38b5d17b5
nikossrp <41998058+nikossrp@users.noreply.github.com>
2021-05-07 11:13:23 +0300

    Update Implementation.c

--- Implementation.c
+++ Implementation.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include "Interface.h"
 
-void InitializeStack(Stack* S)  //o pointer pou deixnei sthn stiva einai NULL, Dhladh arxizoume me kenh stoiva
+void InitializeStack(Stack* S) 
 {
     S->ItemList = NULL;
 }
@@ -12,7 +12,7 @@ int Empty(Stack* S)
     return S->ItemList == NULL;
 }
 
-int Full(Stack* S) //den einai Full ,dhladh den exoume periorismo edw ( kanonika exoume to heap kapoia stigmi tha gemish)
+int Full(Stack* S) 
 {
     return 0;
 }
@@ -34,7 +34,7 @@ void Pop(Stack* S, ItemType* X)
 {
     StackNode* Temp;
 
-    if(S->ItemList == NULL) printf("H stoiva einai kenh den uparxei stoixeio na bgaleis");
+    if(S->ItemList == NULL) printf("Stack is empty");
     else{
         Temp =S->ItemList;
         *X = Temp->Item;
